730cc5aabede85351c8898c3bd532d628062f560
septa-setiawan <septa@icube.us>
2016-01-27 15:19:18 +0700

    create theme mage2

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -41,6 +41,16 @@ module.exports = {
         ],
         dsl: 'less'
     },
+    josef: {
+        area: 'frontend',
+        name: 'Icube/josef',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     backend: {
         area: 'adminhtml',
         name: 'Magento/backend',
